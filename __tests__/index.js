916cd0ceb41eac3481ab6adb56aeaf8d4d1cba16
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2017-02-21 20:00:11 -0500

    Base app with eslint

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,12 +1,13 @@
 import 'react-native';
 import React from 'react';
-import Index from '../index.android.js';
+import CircleCi from '../js/index';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   const tree = renderer.create(
-    <Index />
+    <CircleCi />,
   );
+  expect(tree).toMatchSnapshot();
 });
